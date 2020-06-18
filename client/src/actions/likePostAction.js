36c30e2508594482e0aad7a1e18a3afc5793d177
Hank Liu <hanke.liu@gmail.com>
2020-06-18 17:45:11 -0400

    set heroku url for fetch action in likePostAction.js

--- client/src/actions/likePostAction.js
+++ client/src/actions/likePostAction.js
@@ -3,7 +3,7 @@ export function likePost(postId, voteValue, valueCSRF) {
   return dispatch => {
     dispatch({type: 'UPLOADING_POSTLIKE'});
     return (
-      fetch(`http://localhost:3000/api/v1/posts/${postId}`, {
+      fetch(`https://news-reader-react.herokuapp.com/api/v1/posts/${postId}`, {
 
       method: 'PUT',
       headers: {
@@ -17,7 +17,7 @@ export function likePost(postId, voteValue, valueCSRF) {
       })
       .then(resp => resp.json())
       .then(post => {
-        // console.log(post) //use log to track the fetch request. 
+        // console.log(post) //use log to track the fetch request.
         dispatch({type:'FETCHING_POSTLIKE', payload: post})
       })
       .catch(function(error) {console.log('There has been a problem with your fetch POST operation: ', error.message);})

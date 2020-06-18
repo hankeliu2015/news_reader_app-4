00f0446a615e73baa6885652ddac71555faa3242
Hank Liu <hanke.liu@gmail.com>
2020-06-18 17:29:51 -0400

    add rack-cors configuration to the application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,12 @@ module NewsReader
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'https://news-reader-react.herokuapp.com'
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+      end
+    end
   end
 end

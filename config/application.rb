a3ec1e9fcd67fc0897336c012e052d6884382979
Hank Liu <hanke.liu@gmail.com>
2020-06-18 21:34:40 -0400

    adjust cors settigns in application.rb and testing fetch url in likePostAction.js"

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,9 @@ module NewsReader
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'https://news-reader-react.herokuapp.com'
-        resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head]
+        resource '*',
+        headers: :any,
+        methods: [:get, :post, :patch, :put, :delete, :options, :head]
       end
     end
   end

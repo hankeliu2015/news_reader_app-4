6ee4d391ebaf45a06c8b0c48979f14dfca80b759
Hank Liu <hanke.liu@gmail.com>
2020-06-19 18:08:01 -0400

    correct the fetch url in fetchComments.js

--- client/src/actions/commentFetchAction.js
+++ client/src/actions/commentFetchAction.js
@@ -2,7 +2,7 @@ export default function fetchComments() {
   return dispatch => {
     dispatch({type: 'LOADING_COMMENTS'});
     return (
-      fetch('http://localhost:3000/comments')
+      fetch('/comments')
         .then(resp => resp.json())
         .then(comments => dispatch({type: 'FETCHING_COMMENTS', payload: comments}))
         .catch(function(error) {console.log('There has been a problem with your fetch GET operation: ', error.message);})

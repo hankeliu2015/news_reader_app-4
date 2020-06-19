6d1a8bd6f9675b7ddc3f2f878b935fe8d409a8a5
Hank Liu <hanke.liu@gmail.com>
2020-06-18 23:41:37 -0400

    remove https from cors origin url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://news-reader-react.herokuapp.com'
+    origins 'news-reader-react.herokuapp.com'
     resource '*',
     headers: :any,
     methods: [:get, :post, :patch, :put, :delete, :options, :head]

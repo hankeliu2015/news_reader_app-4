b9791a4f8b21158d6c3a6655a18b57fbcad69e41
Hank Liu <hanke.liu@gmail.com>
2020-06-18 23:27:54 -0400

    reset fetch url in currentUserAction.js

--- client/src/actions/currentUserAction.js
+++ client/src/actions/currentUserAction.js
@@ -3,7 +3,7 @@ export default function currentUserFetch() {
     dispatch({type: 'LOADING_USER'});
 
     return (
-      fetch('http://localhost:3000/api/v1/current_user')
+      fetch('https://news-reader-react.herokuapp.com/api/v1/current_user')
       .then(resq => resq.json())
       .then(user => {
         dispatch({type: 'FETCHING_USER', payload: user })

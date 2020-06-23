1a34fcc3af5718f8a604812cb29db933f9a1c9a4
Hank Liu <hanke.liu@gmail.com>
2020-06-23 14:49:38 -0400

    add fonts-awesome cdn to rails applicaiton.html.erb file

--- client/src/components/noneLoginPostsHeader.js
+++ client/src/components/noneLoginPostsHeader.js
@@ -4,9 +4,11 @@ export default function noneLoginPostsHeader(props) {
   return (
     <div>
       <header className="none-login-posts-header">
-
-        <h1>Dear Customer, welcome to News Feed App</h1>
-        <p>Please Login to access Users Posts</p>
+        <div className="content">
+          <i class="fas fa-laptop-code fa-5x"></i>
+          <h1 className="title">Dear Customer, welcome to News Feed App. Please Login to access Users Posts</h1>
+          <p classname="text"></p>
+        </div>
       </header>
     </div>
   )

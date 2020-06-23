afc375c05f83470bae8618be33188a63dea5aa08
Hank Liu <hanke.liu@gmail.com>
2020-06-23 15:33:17 -0400

    add blur feature to posts page header for none logon users

--- client/src/components/noneLoginPostsHeader.js
+++ client/src/components/noneLoginPostsHeader.js
@@ -4,11 +4,9 @@ export default function noneLoginPostsHeader(props) {
   return (
     <div>
       <header className="none-login-posts-header">
-        <div className="content">
-          <i class="fas fa-laptop-code fa-5x"></i>
+          <i className="fas fa-laptop-code fa-5x"></i>
           <h1 className="title">Dear Customer, welcome to News Feed App. Please Login to access Users Posts</h1>
-          <p classname="text"></p>
-        </div>
+          <p className="text"></p>
       </header>
     </div>
   )

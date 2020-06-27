1ac9f9f7025960aafad2bb52b86be6b101f83004
Hank Liu <hanke.liu@gmail.com>
2020-06-27 08:40:08 -0400

    add extra container for navlinks with flex style to wrap the navlinks contents

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -12,18 +12,16 @@ const linkStyle = {
 
 export default function Navbar() {
     return (
-      <div >
-        <ul className={"nav-links"}>
-          <li>
-            <NavLink to="/">Hacker News Feed</NavLink>
-          </li>
-          <li>
-            <NavLink to="/posts">Users Posts</NavLink>
-          </li>
-          <li>
-            <NavLink to="/newpost">Post Your Story</NavLink>
-          </li>
-        </ul>
+      <div className={"nav-links-container"}>
+        <div className={"nav-links"}>
+          <NavLink to="/">Hacker News Feed</NavLink>
+
+          <NavLink to="/posts">Users Posts</NavLink>
+
+          <NavLink to="/newpost">Post Your Story</NavLink>
+
+        </div>
+
       </div>
     )
 }

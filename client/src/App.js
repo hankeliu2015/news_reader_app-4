476f7c11d49b256cee53f5484b8892dc43b739a4
Hank Liu <hanke.liu@gmail.com>
2020-06-29 22:18:43 -0400

    add about.js component and nav path

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import NewPostBeta from './components/newPostBeta';
 import PostEdit from './components/PostEdit';
 import NewComment from './components/NewComment'
 import Container from 'react-bootstrap/Container';
+import About from './components/about';
 import "react-bootstrap/dist/react-bootstrap.min.js";
 
 function App() {
@@ -33,6 +34,7 @@ function App() {
           <Route exact path="/posts/:id/edit" component={PostEdit} />
           <Route exact path='/comments/:id' component={Comments} />   {/* :id is story_id of the comment */}
           <Route exact path='/postcomments/:id' component={Comments} />   {/* :id is post_id of the comment */}
+          <Route exact path='/about' component={About} />   {/* :id is post_id of the comment */}
         </Container>
 
       </div>

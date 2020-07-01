2369cd942520df32207a738cbe8a7f21676e57ee
Hank Liu <hanke.liu@gmail.com>
2020-07-01 12:15:36 -0400

    render condition is set up for newPost page for login and none login users

--- client/src/components/newPost.js
+++ client/src/components/newPost.js
@@ -33,33 +33,36 @@ class NewPost extends Component {
 
     return (
       <div>
-        {noneLoginPostNewPage}
+        {!userName && noneLoginPostNewPage}
 
-        <form onSubmit={this.handleOnSubmit} className="post-form-container">
-          <section className="post-form-header">
-            <div>
-              {!userName ? <p>Please have a simple user & password <a className="button" href="/users/sign_up">Sign up</a> to post your stories</p> : `Hi ${userName}, You can share your own story`}
-            </div>
-          </section>
-
-          <img src="https://res.cloudinary.com/dcyxkrt7p/image/upload/v1592702004/logo_brush.png"></img>
-          <label>Title</label>
-          <input type="text" name="title" onChange={this.handleOnChange} value={this.state.title}></input>
-
-          <label>Content</label>
-
-          <textarea rows="4" name="content" onChange={this.handleOnChange} value={this.state.content}></textarea>
-
+        {userName &&
           <div>
-            {!userName ? <a className="button" href="/users/sign_up">User Sign Up</a> : <button variant="outline-primary" type="submit">Submit</button>}
+            <form onSubmit={this.handleOnSubmit} className="post-form-container">
+              <section className="post-form-header">
+                <div>
+                  {!userName ? <p>Please have a simple user & password <a className="button" href="/users/sign_up">Sign up</a> to post your stories</p> : `Hi ${userName}, You can share your own story`}
+                </div>
+              </section>
+
+              <img src="https://res.cloudinary.com/dcyxkrt7p/image/upload/v1592702004/logo_brush.png"></img>
+              <label>Title</label>
+              <input type="text" name="title" onChange={this.handleOnChange} value={this.state.title}></input>
+
+              <label>Content</label>
+
+              <textarea rows="4" name="content" onChange={this.handleOnChange} value={this.state.content}></textarea>
+
+              <div>
+                {!userName ? <a className="button" href="/users/sign_up">User Sign Up</a> : <button variant="outline-primary" type="submit">Submit</button>}
+              </div>
+
+            </form>
+
+            <footer className="post-form-footer">
+              <p>NewsReader App, Copyrigth &copy; 2019</p>
+            </footer>
           </div>
-
-        </form>
-
-        <footer className="post-form-footer">
-          <p>NewsReader App, Copyrigth &copy; 2019</p>
-        </footer>
-
+        }
       </div>
     )
   }
